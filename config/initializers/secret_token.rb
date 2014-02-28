6509f93a67803553f0dbcbce51183ea64e942876
Taylor Mitchell <scy0846@yahoo.com>
2014-02-28 17:07:26 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Istockhub::Application.config.secret_key_base = 'bf8af9c3bca42cb4c246422947ab54710b2f90a837d2a0b65e7f6178445d5c06ba9e6bee64654f264c92de20c09b90935f37a3025574b00e3f47bf8250e020ef'
