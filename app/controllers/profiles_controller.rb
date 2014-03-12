0e95c4bf9e98e532985ad41d34ef3026a2755f40
Taylor Mitchell <scy0846@yahoo.com>
2014-03-12 13:16:07 -0400

    commit for heroku

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,15 @@
 class ProfilesController < ApplicationController
 	before_action :signed_in_user,
                 only: [:index, :edit, :update, :destroy, :following, :followers]
+  # before_filter :url_to_lower_case
+
+  # def url_to_lower_case
+  #   params[:id] = params[:id].downcase
+  # end
 
 	def show
 		@user = params[:id] ? User.friendly.find(params[:id]) : current_user
-  		@username = "@" + @user.username
+		@username = "@" + @user.username
 		@posting = Posting.new
 		@allpostings = Posting.all
 	end
